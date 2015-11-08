ed4bbb6ba25257e4a15eb84d9d513f3c047f2799
ngo-va_a <ngo-va_a@epitech.eu>
2015-11-08 16:27:50 +0100

    main merges

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -4,40 +4,7 @@
 #endif
 #include <string>
 
-int main()
+int main(int ac, char **av)
 {
-	Server *s = new Server("192.168.1.70", "4242");
-
-	/*
-	//InterClientCom* com = new InterClientCom(std::string("4343"));
-	InterClientCom* com = new InterClientCom(std::string("192.168.1.28"), std::string("4343"));
-	InterCPacket* packet = new InterCPacket;
-	system("pause");
-	std::cout << "CLIENT START" << std::endl;
-	//std::cout << "SERVER START" << std::endl;
-	std::string str;
-	while (1)
-	{
-		
-		std::cin >> str;
-		if (com->SendData(str))
-		{
-			std::cout << "before" << std::endl;
-			if ((packet = com->ReceiveData()) != nullptr)
-				std::cout << "MESSAGE: " << packet->message << std::endl;
-			std::cout << "after" << std::endl;
-		}
-		else
-			std::cout << "send failed" << std::endl;
-
-		//if (com->TryAccept())
-		//{
-		//	if ((packet = com->ReceiveData()) < 0)
-		//	{
-		//		std::cout << "MESSAGE: " << packet->message << std::endl;
-		//		com->SendData(std::string("super"));
-		//	}
-		//}
-	}
-	*/
+	Server *s = new Server("192.168.1.70", av[1]);
 }
